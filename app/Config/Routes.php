7cb372cb3abe8f1d9d1ddb8f173c57b91b16404f
annddri <andriwidani@gmail.com>
2025-01-03 18:46:44 +0700

    percobaan awal routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/coba', 'Coba::index');
+$routes->get('/coba/index', 'Coba::index');
+$routes->get('/coba/about', 'Coba::about');
+$routes->get('/coba/(:any)', 'Coba::about/$1');
+
+$routes->get('/users', 'Admin\Users::index');
